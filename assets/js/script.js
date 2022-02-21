a5cb5266d175757b75fb6969001f47ea4be26520
Nevan <nev.kd2832@gmail.com>
2022-02-21 18:14:12 -0500

    image location, styling of UVI and rendering of weather data that user inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,3 +35,53 @@ function addCitytoHistory(cityName) {
     }
 }
 
+// This function handles the search after the necessary event occurs
+function handleSubmitSearch(event) {
+    // prevents the page from reloading...
+    event.preventDefault();
+
+    // gets city's name from search bar
+    let cityName = $("#input-search").val().trim();
+
+    // makes search box empty after submitting
+    $("#input-search").val("");
+    // Adds city to search history
+    addCitytoHistory(cityName);
+    // renders the city's weather
+    renderCityWeather(cityName);
+}
+
+// function renders button to display weather data based on city name stored in button
+function handleHistoryBtnClick(event) {
+    if (event.target.matches("button")) {
+      renderCityWeather($(event.target).attr(dataCityAttr));
+    }
+  }
+
+// function gives the image location of the icon used in the OpenWeather API
+function getIcon(code) {
+    return "https://openweathermap.org/img/w/" + code + ".png";
+}
+
+// function changes styling of UV index element in HTML
+function uviElementStyle(uvi) {
+    if (uvi <= 2) {
+        return "background-color: seagreen; color: white;";
+    } else if (uvi <= 5) {
+        return "background-color: yellow; color: black";
+    } else if (uvi <= 7) {
+        return "background-color: darkorange; color: black;";
+    } else if (uvi <= 10) {
+        return "background-color: red; color: white;";
+    } else {
+        return "background-color: orchid; color: black;";
+    }
+}
+
+// function renders weather data for city that user inputs
+function renderCityWeather(cityName) {
+    // Hides info divs on the page since they display nothing yet
+    $("#todays-weather").attr("style", "display: none;");
+    $("#uv-today-element").attr("style", "display: none;");
+    $("#next-weather").attr("style", "display: none;");
+
